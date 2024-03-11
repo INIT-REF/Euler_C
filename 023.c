cb2ef0337a2986434c22f43ecb3c9ff7cddc8ec3
INIT-REF <rufmichan@freenet.de>
2024-03-11 14:39:41 +0100

    Update

---
+++ 023.c
@@ -0,0 +1,55 @@
+// Solution to Project Euler Problem 021
+
+#include <stdio.h>
+
+int divSum(int n) {
+    int dsum = 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            if (i == n / i) {
+                dsum += i;
+            }
+            else {
+                dsum += i + n / i;
+            }
+        }
+    }
+
+    return ++dsum;
+}
+
+int main() {
+
+    int sum = 0;
+    int list[28124] = {0};
+    int isAbNum[28124] = {0};
+
+    for (int i = 0; i < 28124; i++) {
+        if (divSum(i) > i) {
+            isAbNum[i] = 1;
+        }
+    }
+    
+    for (int i = 1; i < 28124; i++) {
+        if (!isAbNum[i]) {
+            continue;
+        }
+
+        for (int j = i; j < 28124 - i; j++) {
+            if (!isAbNum[j]) {
+                continue;
+            }
+            
+            list[i + j] = 1;
+        }
+    }
+
+    for (int i = 0; i < 28124; i++) {
+        sum += i * !list[i];
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
