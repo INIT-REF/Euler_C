961870dbbc1b08e89e8fe58266ff8180b0906989
INIT-REF <rufmichan@freenet.de>
2024-03-12 09:33:14 +0100

    Update

--- 025.c
+++ 025.c
@@ -5,21 +5,25 @@
 int main() {
     int fib1[1000] = {0};
     int fib2[1000] = {0};
-    int carry, tmp, index = 1;
+    int carry, tmp, index = 2, firstsig = 998;
 
     fib1[999] = fib2[999] = 1;
 
 
-    while (fib2[0] == 0) {
+    while (!fib1[0]) {
         carry = 0;
 
-        for (int i = 999; i >= 0; i--) {
+        for (int i = 999; i >= firstsig; i--) {
             fib2[i] += fib1[i] + carry;
             carry = fib2[i] / 10;
             fib2[i] %= 10;
             tmp = fib2[i];
             fib2[i] = fib1[i];
             fib1[i] = tmp;
+        }
+        
+        if (fib1[firstsig]) {
+            firstsig--;
         }
 
         index++;
