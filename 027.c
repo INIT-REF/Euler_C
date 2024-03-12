5717bb3a1cb63b0c7018fa011729dbb1873f126f
INIT-REF <rufmichan@freenet.de>
2024-03-12 10:43:27 +0100

    Update

---
+++ 027.c
@@ -0,0 +1,54 @@
+// Solution to Project Euler Problem 027
+//
+// Using the fact that b needs to be a prime, or else the formula
+// won't result in a prime for n=0.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* primesieve(int limit) {
+
+    int* primes = (int*)malloc(limit * sizeof(int));
+    
+    for (int i = 2; i < limit; i++)
+        primes[i] = 1;
+
+    for (int i = 2; i * i < limit; i++) {
+        if (primes[i]) {
+            for (int j = i * i; j < limit; j += i) {
+                primes[j] = 0;
+            }
+        }   
+    }
+
+    return primes;
+}
+
+
+int main() { 
+    int* primes = primesieve(2000);
+    int n, maxn = 0, p;
+
+    for (int b = 2; b < 1000; b++) {
+        if (!primes[b]) {
+            continue;
+        }
+
+        for (int a = -999; a < 1000; a++) {
+            n = 0;
+
+            while (primes[(n * n) + (a * n) + b]) {
+                n++;
+            }
+
+            if (n > maxn) {
+                maxn = n;
+                p = a * b;
+            }
+        }
+    }    
+
+    printf("%d\n", p);
+
+    return 0;
+}
