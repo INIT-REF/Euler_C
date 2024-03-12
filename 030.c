fdbea7419a9d33b31419fe96d572aa950bf90586
INIT-REF <rufmichan@freenet.de>
2024-03-12 11:36:40 +0100

    Update

---
+++ 030.c
@@ -0,0 +1,28 @@
+// Solution to Project Euler Problem 030
+//
+// Using 6 * 9^5 as the upper limit, because 7 * 9^5 is still a 6-digit number
+
+#include <stdio.h>
+
+int main() {
+    int sum, tmp, p, total = 0;
+
+    for (int i = 2; i < 354294; i++) {
+        sum = 0;
+        tmp = i;
+
+        while (tmp) {
+            p = tmp % 10;
+            sum += p * p * p * p * p;
+            tmp /= 10;
+        }
+
+        if (sum == i) {
+            total += i;
+        }
+    }
+
+    printf("%d\n", total);
+
+    return 0;
+}
