00c713f75eae373637dc138cb33265dc4220a0ac
INIT-REF <rufmichan@freenet.de>
2024-03-12 09:55:01 +0100

    Update

---
+++ 026.c
@@ -0,0 +1,30 @@
+// Solution to Project Euler Problem 026
+
+#include <stdio.h>
+
+int main() {
+    int a, l, maxd, max = 0;
+
+    for (int d = 3; d < 1000; d += 2) {
+        if (d % 5 == 0) {
+            continue;
+        }
+
+        a = 10 % d;
+        l = 1;
+
+        while (a > 1) {
+            l++;
+            a = (a * 10) % d;
+        }
+
+        if (l > max) {
+            max = l;
+            maxd = d;
+        }
+    } 
+
+    printf("%d\n", maxd);
+
+    return 0;
+}
