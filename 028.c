2590e718cfef707ffadcb10d5df9d91cb6ad6319
INIT-REF <rufmichan@freenet.de>
2024-03-12 11:01:16 +0100

    Update

---
+++ 028.c
@@ -0,0 +1,16 @@
+// Solution to Project Euler Problem 028
+
+#include <stdio.h>
+
+int main() {
+    int sum = 1, n = 1, size = 1001;
+
+    for (int i = 4; i < 2 * size + 2; i++) {
+        n += 2 * (i / 4);
+        sum += n;
+    } 
+
+    printf("%d\n", sum);
+
+    return 0;
+}
