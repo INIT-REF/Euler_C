424d84c22a4bc3f910e8fd9868a48c5403fc5220
INIT-REF <rufmichan@freenet.de>
2024-06-28 21:21:34 +0200

    Update

--- 024.c
+++ 024.c
@@ -1,27 +1,13 @@
-// Solution to Project Euler Problem 024
-//
-// Using the "Lehmer Code" to compute the permutation
-
-
 #include <stdio.h>
-
-int fact(int n) {
-    int f = 1;
-
-    for (int i = 2; i <= n; i++) {
-        f *= i;
-    }
-
-    return f;
-}
 
 int main(void) {
     int d, j, t, p = 999999;
     int taken[10] = {0};
+    int fact[10] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 
     for (int i = 9; i >= 0; i--) {
-        d = p / fact(i);
-        p %= fact(i);
+        d = p / fact[i];
+        p %= fact[i];
         t = 0;
         j = 0;
 
