c6be0b54e47597aa9d1373334265930cda12d6a3
INIT-REF <rufmichan@freenet.de>
2024-06-28 13:04:07 +0200

    Update

--- 009.c
+++ 009.c
@@ -1,12 +1,8 @@
-// Solution to Project Euler Problem 009
-//
-// Using Dickson's Method to create pythagorean triplets  
-
 #include <stdio.h>
 
 int main(void) {
 
-    int finished = 0, r = 2, d, p;
+    int finished = 0, r = 2, d, abc;
 
     while (!finished) {
         d = r * r / 2;
@@ -14,14 +10,14 @@ int main(void) {
         for (int i = 1; i * i < d; i++) {
             if (d % i == 0 && 1000 == (3 * r) + (2 * i) + (2 * d / i)) {
                 finished = 1;
-                p = (r + i) * (r + (d / i)) * (r + i + (d / i));
+                abc = (r + i) * (r + (d / i)) * (r + i + (d / i));
             }
         }
 
         r += 2;
     }
 
-    printf("%d\n", p);
+    printf("%d\n", abc);
 
     return 0;
 }
