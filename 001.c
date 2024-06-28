edd59aec36b62990696c6a13a7fca47a84c10923
INIT-REF <rufmichan@freenet.de>
2024-06-28 10:22:51 +0200

    Update

--- 001.c
+++ 001.c
@@ -1,17 +1,10 @@
-// Solution to Project Euler Problem 001
-//
-// Using an alternative to the "standard" way with (if i % 3 == 0 || i % 5 == 0)  
-
 #include <stdio.h>
 
 int main(void) {
-    int sum = 0, s = 0;
-    int steps[7] = {3, 2, 1, 3, 1, 2, 3};
+    int sum = 0;
 
-    for (int i = 3; i < 1000; i += steps[s]) {
-        sum += i;
-        s = ++s % 7;
-    }
+    for (int i = 0; i < 1000; i++)
+        sum += i * !(i % 3 * i % 5);
 
     printf("%d\n", sum);
 
