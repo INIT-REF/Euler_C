ec6248d3a2ca66e4446aab1dbed3a1a4bf699703
INIT-REF <rufmichan@freenet.de>
2024-06-28 11:08:18 +0200

    Update

--- 004.c
+++ 004.c
@@ -1,17 +1,14 @@
-// Solution to Project Euler Problem 004
-
-
 #include <stdio.h>
 
 int isPalindrome(int n) {
-    int tmp = n, reverse = 0;
+    int tmp = n, rev = 0;
 
     while (tmp) {
-        reverse = reverse * 10 + tmp % 10;
+        rev = rev * 10 + tmp % 10;
         tmp /= 10;
     }
 
-    return n == reverse;
+    return n == rev;
 }
 
 
