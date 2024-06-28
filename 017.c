081668b11cc6851f6b6b8cb65e02b878ed685ea3
INIT-REF <rufmichan@freenet.de>
2024-06-28 14:34:37 +0200

    Update

--- 017.c
+++ 017.c
@@ -5,21 +5,19 @@ int main(void) {
     char teens[] = {3, 6, 6, 8, 8, 7, 7, 9, 8, 8};
     char tens[] = {0, 0, 6, 6, 5, 5, 5, 7, 6, 6};
     char hundreds[] = {0, 10, 10, 12, 11, 11, 10, 12, 12, 11};
-    int n, sum = 11; //for 1000 
+    int sum = 11; //for 1000 
     
     sum += 9 * 99 * 3; //for 9 * 99 * "and" in 100s 
                        
     for (int i = 1; i < 1000; i++) {
-        n = i;
-
-        if (n % 100 < 10)
-            sum += ones[n % 10];
-        else if (n % 100 < 20)
-            sum += teens[n % 10];
+        if (i % 100 < 10)
+            sum += ones[i % 10];
+        else if (i % 100 < 20)
+            sum += teens[i % 10];
         else
-            sum += tens[n % 100 / 10] + ones[n % 10];
+            sum += tens[i % 100 / 10] + ones[i % 10];
 
-        sum += hundreds[n / 100];
+        sum += hundreds[i / 100];
     }
 
     printf("%d\n", sum);
