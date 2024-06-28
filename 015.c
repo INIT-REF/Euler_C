ef6672d138834374f27ba14382145611f5818fbb
INIT-REF <rufmichan@freenet.de>
2024-06-28 13:53:43 +0200

    Update

--- 015.c
+++ 015.c
@@ -1,10 +1,3 @@
-// Solution to Project Euler Problem 015
-//
-// The general solution for this problem is to calculate "n choose k"
-// with k being the width of the square grid and n being 2 * k. We can
-// then use the multiplicative formula, simplified for the specific
-// problem.
-
 #include <stdio.h>
 
 int main(void) {
